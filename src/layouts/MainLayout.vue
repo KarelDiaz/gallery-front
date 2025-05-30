3ddba2800c755644a8851ba9b08b682679ea2a8f
Luis Manzano <catinlamh@gmail.com>
2025-05-29 22:04:13 -0500

    feat: implement dynamic category navigation in MainLayout.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,38 @@
+<script setup>
+import { ref, onMounted } from 'vue'
+import { api } from 'src/boot/axios'
+
+const categories = ref([])
+
+onMounted(async () => {
+  try {
+    const c = await api.get('/categories?populate=*')
+    categories.value = c.data.data
+  } catch {
+    alert('Ocurrio un error al cargar la barra de navegación')
+  }
+})
+</script>
+
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
         <!-- aqui van las categorias -->
-        <q-btn flat to="slug" label="Nombre de la categoria aqui" />
+        <div class="main-layout__categories">
+          <div
+            class="main-layout__categories-item"
+            v-for="category in categories"
+            :key="category.id"
+          >
+            <q-btn
+              flat
+              :to="category.attributes.slug"
+              :label="category.attributes.name"
+              class="main-layout__categories-btn"
+            />
+          </div>
+        </div>
       </q-toolbar>
     </q-header>
     <q-page-container class="page-container">
@@ -12,12 +41,17 @@
   </q-layout>
 </template>
 
-<script setup>
-</script>
-
 <style scoped>
 .page-container {
   height: 100dvh;
+  width: 100%;
+}
+
+.main-layout__categories {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   width: 100%;
 }
 </style>
